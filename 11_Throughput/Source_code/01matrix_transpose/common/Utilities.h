ec4869cd87f66d2a516c833d083eeb03cfe71136
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-11-27 17:38:18 +0800

    fix formula: 10e9 -> 1e9

--- 11_Throughput/Source_code/01matrix_transpose/common/Utilities.h
+++ 11_Throughput/Source_code/01matrix_transpose/common/Utilities.h
@@ -15,17 +15,14 @@ void bandwidth(int n, float time){
 
         //int cudaDeviceProp::memoryClockRate [inherited]
         //Peak memory clock frequency in kilohertz
-        //should be prop.memoryClockRate * 10e3f? (from KHz to Hz)
+        //should be prop.memoryClockRate * 10e3f? (from KHz to Hz) -> no, because 10e2f is 1000
         
         //int cudaDeviceProp::memoryBusWidth [inherited]
         //Global memory bus width in bits
-        //should be time * 10e-3f?(from ms to s)
+        //should be time * 10e-3f?(from ms to s) -> no, because 10e-4f is 0.001
 
-	//tbw = (((prop.memoryClockRate * 10e2f) * (prop.memoryBusWidth/8))*2)/10e9f;
-	//efbw = ((n*n*4*2)/10e9)/(time*10e-4f);
-
-	tbw = (((prop.memoryClockRate * 10e3f) * (prop.memoryBusWidth/8))*2)/10e9f;
-	efbw = ((n*n*4*2)/10e9)/(time*10e-3f);
+	tbw = (((prop.memoryClockRate * 1e3f) * (prop.memoryBusWidth/8))*2)/1e9f;
+	efbw = ((n*n*4*2)/1e9)/(time*1e-3f);
 	ram = (efbw / tbw)*100;
 
 	printf("TBw = %.3f GB/s, EBw = %.3f GB/s, RAM Utilization = %.2f %%  \n", tbw, efbw, ram);
