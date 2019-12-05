81b6f6194b2c13dd470b3eb7fd74979c24af13cf
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-12-05 10:22:28 +0800

    disable FMA and add assertion

--- 12_Streams/Source_code/01single_stream/01single_stream.cu
+++ 12_Streams/Source_code/01single_stream/01single_stream.cu
@@ -3,6 +3,8 @@
 #include "common/Error.h"
 #include "common/GpuTimer.h"
 #include "common/Vector.h"
+#include <assert.h>
+#include <limits> //std::numeric_limits<float>::epsilon()
 
 #define N 10000
 #define DIMGRID 10
@@ -17,9 +19,18 @@ __host__ __device__ float function1(float x)
 	return x*x;
 }
 
+__device__ float function1_d(float x){
+        return __fmul_rn(x, x);
+}
+
 __host__ __device__ float function2(float x)
 {
 	return sinf(x);
+}
+
+__device__ float function2_d(float x)
+{
+	return __sinf(x);
 }
 
 __global__ void functionKernel1( Vector<float> d_a, int n)
@@ -30,8 +41,10 @@ __global__ void functionKernel1( Vector<float> d_a, int n)
 	dx = (XMAX - (XMIN)) / ((float) N-1);
 	while (i < n)
 	{
-		x = XMIN + i*dx;
-		d_a.setElement( i, function1(x) );
+		//x = XMIN + i*dx;
+		x = __fadd_rn(XMIN, __fmul_rn(i, dx));
+		//d_a.setElement( i, function1(x) );
+		d_a.setElement( i, function1_d(x) );
 		i += blockDim.x*gridDim.x;
 	}
 	
@@ -46,8 +59,10 @@ __global__ void functionKernel2( Vector<float> d_a, int n)
 	
 	while (i < n)
 	{
-		x = XMIN + i*dx;
-		d_a.setElement( i, function2(x) );
+		//x = XMIN + i*dx;
+		x = __fadd_rn(XMIN, __fmul_rn(i, dx));
+		//d_a.setElement( i, function2(x) );
+		d_a.setElement( i, function2_d(x) );
 		i += blockDim.x*gridDim.x;
 	}
 	
@@ -74,6 +89,8 @@ void onDevice(Vector<float> h_a, Vector<float> h_b)
 	HANDLER_ERROR_MSG("kernel panic!!!"); 	
 	
 	//cudaThreadSynchronize(); 
+        //https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__THREAD__DEPRECATED.html#group__CUDART__THREAD__DEPRECATED_1g86ba4d3d51221f18a9a13663e6105fc1
+        //cudaThreadSynchronize is deprecated
         cudaDeviceSynchronize();
 	
 
@@ -124,10 +141,21 @@ void test(){
     // call device configuration
     onDevice(h_a, h_b);
 
+/*
     printf("square:\n");
     h_a.print();
     printf("sin:\n");
     h_b.print();
+*/         
+
+    for(int i = 0; i < N; i++){
+        float x, dx;
+        dx = (XMAX - (XMIN)) / ((float) N-1);
+        x = XMIN + i*dx;
+
+        assert(h_a.getElement(i) == function1(x));
+        assert(std::fabs(h_b.getElement(i) - function2(x)) <= std::numeric_limits<float>::epsilon() * 10);
+    }
 
     // free host memory
     free(h_a.elements);
