e1c95175c9d2c12bf994cdf1d3e2791c91018064
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-12-05 17:32:45 +0800

    add note and result, fix undefined sleep

--- 14_Multiple_GPUs/Source_code/01hello_pthreads/01hello_pthreads.cu
+++ 14_Multiple_GPUs/Source_code/01hello_pthreads/01hello_pthreads.cu
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h> //sleep
 
 void *hello (void *arg) {
 	int *ptrToThreadNumber = (int *)arg;
@@ -14,13 +15,38 @@ int main(void) {
 
 	//pthread_create creates a new thread and makes it executable. 
 	//This routine can be called any number of times from anywhere within your code.
-	
+	/*
+        int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+                          void *(*start_routine) (void *), void *arg);
+        The pthread_create() function starts a new thread in the calling
+        process.  The new thread starts execution by invoking
+        start_routine(); arg is passed as the sole argument of
+        start_routine().
+        If attr is NULL, then the thread is created with
+        default attributes.
+        */
 	pthread_create(&tid,NULL,hello,&threadNum);
 
 	//"Joining" is one way to accomplish synchronization between threads
 	//The pthread_join() subroutine blocks the calling thread until the 
 	//specified threadid thread terminates.
+        /*
+        int pthread_join(pthread_t thread, void **retval);
+        If retval is not NULL, then pthread_join() copies the exit status of
+        the target thread into the location pointed to by retval. 
+        */
 	pthread_join(tid,NULL);
 	printf("------------------------\n");
 	return(0);
 }
+
+/*
+result:
+HelloThread 1
+------------------------
+*/
+
+/*
+result without pthread_join:
+------------------------
+*/
