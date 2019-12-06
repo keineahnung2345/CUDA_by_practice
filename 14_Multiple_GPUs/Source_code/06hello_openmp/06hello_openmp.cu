f901c2ff8cf6807853bd93ea7db36e62dcdd19fd
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-12-06 11:16:01 +0800

    add note

--- 14_Multiple_GPUs/Source_code/06hello_openmp/06hello_openmp.cu
+++ 14_Multiple_GPUs/Source_code/06hello_openmp/06hello_openmp.cu
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <omp.h>
 
+//nvcc   -Xcompiler -fopenmp 06hello_openmp.cu   -o exe
+//https://docs.nvidia.com/cuda/cuda-compiler-driver-nvcc/index.html#options-for-passing-specific-phase-options-compiler-options
+//-Xcompiler: passing specific options directly to compiler/preprocessor(gcc or g++ on Linux)
+//-fopenmp: one of gcc's C Language Options
 
 int main() {
 
@@ -16,3 +20,18 @@ int main() {
     printf("GoodBye World\n");
 
 }
+
+/*
+result:
+
+Hello World
+Iter:0
+Iter:4
+Iter:3
+Iter:2
+Iter:5
+Iter:1
+GoodBye World
+
+note that the order will change every time it's executed
+*/
